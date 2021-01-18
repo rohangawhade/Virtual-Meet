b6af2222e071ae0802e3bce1dafe530bf72e20b3
rohangawhade <gawhaderohan@gmail.com>
2021-01-18 12:13:20 +0530

    Added styling to the components

--- public/js/script.js
+++ public/js/script.js
@@ -1,5 +1,4 @@
 const socket = io("/");
-
 const videoGrid = document.getElementById('video-grid');
 const myVideo = document.createElement('video');
 myVideo.muted = true;
@@ -13,22 +12,35 @@ var peer = new Peer(undefined, {
 
 navigator.mediaDevices.getUserMedia({
     video: true,
-    audio: true
+    audio: false
 }).then(stream => {
     myVideoStream = stream;
     addVideoStream(myVideo, myVideoStream);
+
+    peer.on('call', call => {
+        call.answer(stream)
+        const video = document.createElement('video')
+        call.on('stream', userVideoStream => {
+            addVideoStream(video, userVideoStream);
+        })
+    })
+    
+    socket.on('user-connected', (userId) => {
+        connectToNewUser(userId, stream);
+    });
 })
 
 peer.on('open', id => {
     socket.emit('join-room', ROOM_ID, id);
 })
 
-socket.on('user-connected', (userId) => {
-    connectToNewUser(userId);
-});
 
-const connectToNewUser = (userId) => {
-    console.log(userId);
+const connectToNewUser = (userId, stream) => {
+    const call = peer.call(userId, stream);
+    const video = document.createElement('video');
+    call.on('stream', userVideoStream => {
+        addVideoStream(video, userVideoStream);
+    })
 }
 
 const addVideoStream = (video, stream) => {
